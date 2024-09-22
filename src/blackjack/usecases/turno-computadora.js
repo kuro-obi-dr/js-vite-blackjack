e82276534f38b37cd346d63fa2e3cbab0bfe20a9
David Ramirez <kuro.obi.dr@gmail.com>
2024-09-21 23:44:22 -0600

    First commit

---
+++ src/blackjack/usecases/turno-computadora.js
@@ -0,0 +1,42 @@
+import { pedirCarta, valorCarta, crearCartaHTML } from "./";
+
+/**
+ * Turno de la computadora
+ * @param {Number} puntosMinimos 
+ * @param {HTMLElement} puntosHTML
+ * @param {HTMLElement} divCartasComputadora
+ * @param {Array<String>} deck 
+ */
+export const turnoComputadora = ( puntosMinimos, puntosHTML, divCartasComputadora, deck = [] ) => {
+
+    if( !puntosMinimos ) throw new Error('Puntos minimos con necesarios');
+
+    let puntosComputadora = 0;
+
+    do {
+        const carta = pedirCarta( deck );
+
+        puntosComputadora = puntosComputadora + valorCarta( carta );
+        puntosHTML.innerText = puntosComputadora;
+        
+        const imgCarta = crearCartaHTML( carta );
+        divCartasComputadora.append( imgCarta );
+
+        if( puntosMinimos > 21 ) {
+            break;
+        }
+
+    } while(  (puntosComputadora < puntosMinimos)  && (puntosMinimos <= 21 ) );
+
+    setTimeout(() => {
+        if( puntosComputadora === puntosMinimos ) {
+            alert('Nadie gana :(');
+        } else if ( puntosMinimos > 21 ) {
+            alert('Computadora gana')
+        } else if( puntosComputadora > 21 ) {
+            alert('Jugador Gana');
+        } else {
+            alert('Computadora Gana')
+        }
+    }, 100 );
+}
